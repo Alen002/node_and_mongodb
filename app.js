240ebd4677d281f13ba03f4cdd6967e0c3268058
Alen <alechkr@gmail.com>
2020-08-10 17:28:14 +0200

    route for persons

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const port = 9000;
-const url = 'mongodb://localhost/PersonDB';
 const app = express();
+
+
+
+// MongoDB configuration
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost/PersonDB';
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const con = mongoose.connection;
@@ -10,13 +14,11 @@ const con = mongoose.connection;
 // Whenever connection is open, a callback function will be executed
 con.on('open', () => console.log('Connected to mongodb'));
 
+const personRouter = require('./routes/persons');
+app.use('/persons', personRouter);
+
+// Run server
 app.listen(port, (err) => { 
     if (err) console.log("Error in server setup"); 
     console.log("Server listening on Port", port);
 });
-
-// Routes
-app.get('/', (req, res) => {         
-    res.send('express is working');
-});
-
