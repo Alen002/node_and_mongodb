33eef02005c8b0b08a18c9efab9c62498652683c
Alen <alechkr@gmail.com>
2020-08-11 15:00:57 +0200

    display single person by id

--- routes/persons.js
+++ routes/persons.js
@@ -3,11 +3,25 @@ const express = require('express');
 const router = express.Router();
 const Person = require('../models/person');
 
+// Displays all collections in the db
 router.get('/', async(req, res) => {  
     try {
-        console.log('Persons requested');  
+        console.log('All persons in the db requested');  
         const persons = await Person.find();
         res.json(persons);
+    } 
+    catch(err) {
+        console.log('There has been an error');
+        res.send('There has been an error');
+    }
+});
+
+// Displays one collection in the db
+router.get('/:id', async(req, res) => {  
+    try {
+        console.log('Single person requested from the db');  
+        const person = await Person.findById(req.params.id);
+        res.json(person);
     } 
     catch(err) {
         console.log('There has been an error');
